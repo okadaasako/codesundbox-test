6069626e575febdddc63771f1af32d8760390992
okadaasako <61569308+okadaasako@users.noreply.github.com>
2021-03-25 23:33:21 +0900

    jsのおさらいなど

--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
-import "./styles.css";
+// const nameArr = ["田中", "山田", "岡田"];
+// for(let index = 0; index < nameArr.length; index++){
+//   console.log(`${index}番目は${nameArr[index]}です。`);
+// }
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const nameArr2 = nameArr.map((name) => {
+//   return name
+// });
+
+// console.log(nameArr2);
+
+// nameArr.map((name, index)=>console.log(`${index}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNauArr = numArr.filter((num)=>{
+//   return num % 2 === 0;
+// });
+
+// console.log(newNauArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "岡田") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+
+// console.log(newNameArr);
+
+// const val1 = 1 > 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = 1300;
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください。';
+// console.log(formattedNum);
+
+// const checkSum = (num1 , num2) => {
+//   return num1 + num2 > 100 ? '100を超えています':'許容範囲内です。';
+// }
+// console.log(checkSum(50, 40));
+
+// const flag1 = true;
+// const flag2 = false;
+
+// if(flag1 || flag2) {
+//   console.log('1か2はtrueになります。');
+// }
+
+// const num = null;
+// const fee = num || '金額未設定です';
+
+// console.log(fee);
